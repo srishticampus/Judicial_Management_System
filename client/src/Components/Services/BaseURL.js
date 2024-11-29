7c91b72d57df81b1546a167e0ff45a0f2cfe2c5d
soumyasics <soumya.sics@gmail.com>
2024-11-29 16:40:00 +0530

    sprint2

--- client/src/Components/Services/BaseURL.js
+++ client/src/Components/Services/BaseURL.js
@@ -1,9 +1,9 @@
 
 
-export const IMG_BASE_URL = 'http://localhost:4048/';
-export const API_BASE_URL = 'http://localhost:4048/judisys_api';
+// export const IMG_BASE_URL = 'http://localhost:4048/';
+// export const API_BASE_URL = 'http://localhost:4048/judisys_api';
 
-// export const IMG_BASE_URL = 'http://hybrid.srishticampus.in';
-// export const API_BASE_URL = 'https://hybrid.srishticampus.in/judisys_api';
+export const IMG_BASE_URL = 'http://hybrid.srishticampus.in';
+export const API_BASE_URL = 'https://hybrid.srishticampus.in/judisys_api';
 
 
